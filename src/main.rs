50311568ed4c6137be6039914564f76f3719b31b
syamimhazmi <syamimhazmi@gmail.com>
2024-01-11 00:33:45 +0700

    rust functions parameters

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(5);
 
-    another_function();
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function() {
-    println!("Another function");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char)
+{
+    println!("The measurement is: {value}{unit_label}");
 }
